1d7b67242376425cd72ad95fa2528efbbb69cd46
krasi <krasimir.xristov@gmail.com>
2024-07-18 20:29:53 +0200

    create the home page

--- frontend/src/pages/ControllBook.tsx
+++ frontend/src/pages/ControllBook.tsx
@@ -119,7 +119,7 @@ const ControllBook: React.FC = () => {
         <h1 className='text-red-800 font-extrabold  justify-center text-center uppercase py-4 text-2xl  decoration-red-800  md:text-6xl'>
           {t('controllBook')}
         </h1>
-        <p className='text-lg text-justify font-bold mx-4 my-2'>
+        <p className='text-lg  text-justify font-bold mx-4 my-2'>
           {t('cbDescription')}
         </p>
         <hr className='border-t border-gray-400 mx-4 my-4' />
