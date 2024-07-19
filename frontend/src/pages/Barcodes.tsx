bd28f612f5f5527e31fc0450c94d9537f9f7c7dc
krasi <krasimir.xristov@gmail.com>
2024-07-19 18:04:04 +0200

    add bg color to the barcode component slate 200

--- frontend/src/pages/Barcodes.tsx
+++ frontend/src/pages/Barcodes.tsx
@@ -39,27 +39,29 @@ const Barcodes: React.FC = () => {
     },
   ];
   return (
-    <div>
-      {barcodesInfo.map((row, index) => (
-        <div key={index} className='row'>
-          <h2 className='justify-center text-center mt-5 text-3xl font-extrabold bg-yellow-500 text-red-800 p-2  uppercase'>
-            {row.title}
-          </h2>
-          <div className='flex justify-center my-4'>
-            <img
-              src={row.image}
-              alt={row.title}
-              width={400} // Запазете тези стойности за по-малки екрани
-              height={400} // Запазете тези стойности за по-малки екрани
-              className='w-96 h-96 lg:w-[500px] lg:h-[500px]' // Добавете класове за големи екрани
-            />
+    <div className='bg-slate-200'>
+      <div>
+        {barcodesInfo.map((row, index) => (
+          <div key={index} className='row'>
+            <h2 className='justify-center text-center mt-5 text-3xl font-extrabold bg-yellow-500 text-red-800 p-2  uppercase'>
+              {row.title}
+            </h2>
+            <div className='flex justify-center my-4'>
+              <img
+                src={row.image}
+                alt={row.title}
+                width={400} // Запазете тези стойности за по-малки екрани
+                height={400} // Запазете тези стойности за по-малки екрани
+                className='w-96 h-96 lg:w-[500px] lg:h-[500px]' // Добавете класове за големи екрани
+              />
+            </div>
+            <p className='justify-center text-center text-2xl font-bold'>
+              {row.description}
+            </p>
+            <hr className='border-t border-gray-400 mx-4 my-4' />
           </div>
-          <p className='justify-center text-center text-2xl font-bold'>
-            {row.description}
-          </p>
-          <hr className='border-t border-gray-400 mx-4 my-4' />
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
