4688246d1a7c3fa8c2bc6478ca0ae9b1c2db20e7
krasi <krasimir.xristov@gmail.com>
2024-07-19 19:11:27 +0200

    create the bartcode page

--- frontend/src/pages/Barcodes.tsx
+++ frontend/src/pages/Barcodes.tsx
@@ -7,15 +7,17 @@ import firmaClose from '../assets/barcodes/firmaClose.jpg';
 import give from '../assets/barcodes/give.jpg';
 import lagern from '../assets/barcodes/lagern.jpg';
 import streetClose from '../assets/barcodes/streetclose.jpg';
+import ListingCard from '../components/ListingCard';
+import TitleAndDescripion from '../components/TitleAndDescripion';
 
 const Barcodes: React.FC = () => {
   const { t } = useTranslation();
 
   const barcodesInfo = [
     {
-      title: `${t('cancel')}`,
+      title: `${t('cancelTitle')}`,
       image: abruch,
-      description: 'abruch',
+      description: `${t('cancelDesc')}`,
     },
     {
       title: 'Firma Close',
@@ -41,27 +43,21 @@ const Barcodes: React.FC = () => {
   return (
     <div className='bg-slate-200'>
       <div>
-        {barcodesInfo.map((row, index) => (
-          <div key={index} className='row'>
-            <h2 className='justify-center text-center mt-5 text-3xl font-extrabold bg-yellow-500 text-red-800 p-2  uppercase'>
-              {row.title}
-            </h2>
-            <div className='flex justify-center my-4'>
-              <img
-                src={row.image}
-                alt={row.title}
-                width={400} // Запазете тези стойности за по-малки екрани
-                height={400} // Запазете тези стойности за по-малки екрани
-                className='w-96 h-96 lg:w-[500px] lg:h-[500px]' // Добавете класове за големи екрани
-              />
-            </div>
-            <p className='justify-center text-center text-2xl font-bold'>
-              {row.description}
-            </p>
-            <hr className='border-t border-gray-400 mx-4 my-4' />
-          </div>
-        ))}
+        <TitleAndDescripion
+          title={t('barcodesTitle')}
+          description={t('barcodesDesc')}
+        />
       </div>
+      {barcodesInfo.map((row, index) => (
+        <div>
+          <ListingCard
+            key={index}
+            title={row.title}
+            image={row.image}
+            description={row.description}
+          />
+        </div>
+      ))}
     </div>
   );
 };
