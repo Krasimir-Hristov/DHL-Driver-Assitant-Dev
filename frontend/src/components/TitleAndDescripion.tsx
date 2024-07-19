06da3008e8f785fd3315e25781196c35195faf62
krasi <krasimir.xristov@gmail.com>
2024-07-19 18:47:32 +0200

    change the style in the Title and desc component

--- frontend/src/components/TitleAndDescripion.tsx
+++ frontend/src/components/TitleAndDescripion.tsx
@@ -14,7 +14,9 @@ const TitleAndDescripion: React.FC<TitleAndDescripion> = ({
       <h1 className='text-red-800 font-extrabold  justify-center text-center uppercase py-4 text-2xl  decoration-red-800  md:text-6xl'>
         {title}
       </h1>
-      <p className='text-lg  text-justify font-bold mx-4 my-2'>{description}</p>
+      <p className='text-lg  text-center justify-center font-bold mx-4 my-2'>
+        {description}
+      </p>
       <hr className='border-t border-gray-400 mx-4 my-4' />
     </div>
   );
