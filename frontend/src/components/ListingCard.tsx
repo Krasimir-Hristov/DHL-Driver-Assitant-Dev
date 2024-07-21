ee9bbbe6e2a56d0f519df0040c3b88b2c7f0105c
krasi <krasimir.xristov@gmail.com>
2024-07-21 20:30:03 +0200

    refactor: Update ListingCard image property to allow null values

--- frontend/src/components/ListingCard.tsx
+++ frontend/src/components/ListingCard.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
 
-type ListingCard = {
+type ListingCardProps = {
   title: string;
-  image: string;
+  image: string | null;
   description: string;
 };
 
-const ListingCard: React.FC<ListingCard> = ({ title, image, description }) => {
+const ListingCard: React.FC<ListingCardProps> = ({
+  title,
+  image,
+  description,
+}) => {
   return (
     <div className='row'>
       <h2 className='justify-center text-center mt-5 text-3xl font-extrabold bg-yellow-500 text-red-800 p-2 uppercase'>
         {title}
       </h2>
       <div className='flex justify-center my-4'>
-        <img
-          src={image}
-          alt={title}
-          width={400} // Запазете тези стойности за по-малки екрани
-          height={400} // Запазете тези стойности за по-малки екрани
-          className='w-96 h-96 lg:w-[500px] lg:h-[500px]' // Добавете класове за големи екрани
-        />
+        {image && (
+          <img
+            src={image}
+            alt={title}
+            width={400} // Keep these values for smaller screens
+            height={400} // Keep these values for smaller screens
+            className='w-96 h-96 lg:w-[500px] lg:h-[500px]' // Add classes for larger screens
+          />
+        )}
       </div>
       <p className='justify-center text-center text-2xl font-bold'>
         {description}
