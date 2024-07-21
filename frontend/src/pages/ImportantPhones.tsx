2014b5c1f7e0eb106ca99a69f484d86fff5d4bcf
krasi <krasimir.xristov@gmail.com>
2024-07-21 20:14:33 +0200

    chore: Add TitleAndDescripion component to ImportantPhones page

--- frontend/src/pages/ImportantPhones.tsx
+++ frontend/src/pages/ImportantPhones.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { depots } from '../constants';
+import { useTranslation } from 'react-i18next';
+import TitleAndDescripion from '../components/TitleAndDescripion';
+
 const ImportantPhones: React.FC = () => {
-  return <div>ImportantPhones</div>;
+  const { t } = useTranslation();
+  return (
+    <div className='bg-slate-200'>
+      <div>
+        <TitleAndDescripion
+          title={t('telephonesTitle')}
+          description={t('telephonesDesc')}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ImportantPhones;
