8ebd5f204a140dbd9c1ffc629c4157d22a9f451e
krasi <krasimir.xristov@gmail.com>
2024-07-22 20:07:11 +0200

    refactor: Update depot telephone details and translations

--- frontend/src/components/PhonesDetails.tsx
+++ frontend/src/components/PhonesDetails.tsx
@@ -15,60 +15,59 @@ interface Depot {
   telephones: Telephone[];
 }
 
-const depotsTelephones: { [key: string]: Depot } = {
-  köngen: {
-    title: 'Depo Köngen',
-    description: 'Robert-Bosch-Str. 20 73257 Köngen',
-    telephones: [
-      {
-        name: 'Главен офис',
-        number: '+49 123 456 7890',
-        info: 'Звънете на този номер за общи въпроси.',
-      },
-      {
-        name: 'Спешен',
-        number: '+49 123 456 7891',
-        info: 'Звънете на този номер само при спешни случаи.',
-      },
-    ],
-  },
-  waiblingen: {
-    title: 'Склад 2',
-    description: 'Това е описанието за Склад 2.',
-    telephones: [
-      {
-        name: 'Главен офис',
-        number: '+49 987 654 3210',
-        info: 'Звънете на този номер за общи въпроси.',
-      },
-      {
-        name: 'Спешен',
-        number: '+49 987 654 3211',
-        info: 'Звънете на този номер само при спешни случаи.',
-      },
-    ],
-  },
-  kircheim: {
-    title: 'Склад 3',
-    description: 'Това е описанието за Склад 3.',
-    telephones: [
-      {
-        name: 'Главен офис',
-        number: '+49 555 666 7777',
-        info: 'Звънете на този номер за общи въпроси.',
-      },
-      {
-        name: 'Спешен',
-        number: '+49 555 666 7778',
-        info: 'Звънете на този номер само при спешни случаи.',
-      },
-    ],
-  },
-};
-
 const PhonesDetails: React.FC = () => {
   const { id } = useParams<{ id: string }>();
   const { t } = useTranslation();
+  const depotsTelephones: { [key: string]: Depot } = {
+    köngen: {
+      title: 'Depo Köngen',
+      description: 'Robert-Bosch-Str. 20 73257 Köngen',
+      telephones: [
+        {
+          name: `${t('manager')}`,
+          number: '+49 7024 98301 552',
+          info: 'Звънете на този номер за общи въпроси.',
+        },
+        {
+          name: 'Спешен',
+          number: '+49 123 456 7891',
+          info: 'Звънете на този номер само при спешни случаи.',
+        },
+      ],
+    },
+    waiblingen: {
+      title: `${t('manager')}`,
+      description: 'Това е описанието за Склад 2.',
+      telephones: [
+        {
+          name: `${t('manager')}`,
+          number: '+49 987 654 3210',
+          info: 'Звънете на този номер за общи въпроси.',
+        },
+        {
+          name: 'Спешен',
+          number: '+49 987 654 3211',
+          info: 'Звънете на този номер само при спешни случаи.',
+        },
+      ],
+    },
+    kircheim: {
+      title: 'Склад 3',
+      description: 'Това е описанието за Склад 3.',
+      telephones: [
+        {
+          name: 'Главен офис',
+          number: '+49 555 666 7777',
+          info: 'Звънете на този номер за общи въпроси.',
+        },
+        {
+          name: 'Спешен',
+          number: '+49 555 666 7778',
+          info: 'Звънете на този номер само при спешни случаи.',
+        },
+      ],
+    },
+  };
   const depot = depotsTelephones[id as keyof typeof depotsTelephones];
 
   if (!id || !depot) {
